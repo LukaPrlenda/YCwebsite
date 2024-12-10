3c7bd2e5de814fda6103f3a6b0ed7b7c81ef3583
LukaPrlenda <luka.prlenda@stu.ibu.edu.ba>
2024-12-11 00:01:01 +0100

    Adding js

--- YCwebsite_Main.js
+++ YCwebsite_Main.js
@@ -1,1 +1,23 @@
+function li_punat_text() {
+    const punat =document.getElementById("li_punat");
+    const text_punat="Punat "+"~City on Krk island~";
+    if (punat.innerHTML==text_punat){
+        punat.innerHTML="Punat";
+    }
+    else{
+        punat.innerHTML=text_punat
+    }
+}
 
+
+function add_description1(name,text,id1){
+    event.preventDefault();
+    const t1=document.getElementById(id1);
+    const t2="<p>"+text+"</p>";
+    if (t1.innerHTML==name+t2){
+        t1.innerHTML=name;
+    }
+    else{
+        t1.innerHTML=name+t2;
+    }
+}
