b9709ac00027b00395a86a63713467975886bd0f
LukaPrlenda <luka.prlenda@stu.ibu.edu.ba>
2024-12-15 22:33:40 +0100

    Dark theme

--- YCwebsite_Main.js
+++ YCwebsite_Main.js
@@ -48,6 +48,7 @@ function theme_button(){
             t1.addEventListener("mouseout",function(){light_theme_btn(t1,"burlywood",dark_bck_color);})
     
             css1.style.background="white";
+            colore_switch("navy");
         }
     
         else{
@@ -57,6 +58,7 @@ function theme_button(){
             t1.addEventListener("mouseout",function(){light_theme_btn(t1,"burlywood","white");})
             
             css1.style.background=dark_bck_color;
+            colore_switch("lightblue");
         }
     }
 }
@@ -64,4 +66,56 @@ function theme_button(){
 function light_theme_btn(t1,color1,color2){
     t1.style.color=color1;
     t1.style.backgroundColor=color2;
+}
+
+const theme1_id=["theme1,0","theme1,1","theme1,2","theme1,3","theme1,4","theme1,5","theme1,6","theme1,7","theme1,8","theme1,9","theme1,10",
+    "locations",
+]
+const theme1_class=["title2_2",
+    "section_line",
+]
+
+const theme2_id=["theme2.0","theme2.1","theme2.2","theme2.3","theme2.4","theme2.5","theme2.6",
+    "charter_button",
+    "about_us",
+]
+const theme2_class=["theme2",
+]
+
+function colore_switch(color1){
+    for(let i=0;i<theme1_id.length;i++){
+        document.getElementById(theme1_id[i]).style.color=color1;
+
+    }
+    for(let i=0;i<theme1_class.length;i++){
+        const theme_i=document.getElementsByClassName(theme1_class[i]);
+        for(let j=0;j<theme_i.length;j++){
+            theme_i[j].style.color=color1;
+        }
+    }
+
+    for(let i=0;i<theme2_id.length;i++){
+        document.getElementById(theme2_id[i]).style.backgroundColor=color1;
+        if(theme2_id[i]!="about_us"){
+            document.getElementById(theme2_id[i]).addEventListener("mouseover", function(){theme_btn_clicked_id(theme2_id[i],"burlywood")});
+            document.getElementById(theme2_id[i]).addEventListener("click", function(){theme_btn_clicked_id(theme2_id[i],"rgb(214, 167, 105)")});
+            document.getElementById(theme2_id[i]).addEventListener("mouseout", function(){theme_btn_clicked_id(theme2_id[i],color1)});
+        }
+    }
+    for(let i=0;i<theme2_class.length;i++){
+        const theme_i=document.getElementsByClassName(theme2_class[i]);
+        for(let j=0;j<theme_i.length;j++){
+            theme_i[j].style.backgroundColor=color1;
+            theme_i[j].addEventListener("mouseover", function(){theme_btn_clicked_class(theme_i[j],"burlywood")});
+            theme_i[j].addEventListener("click", function(){theme_btn_clicked_class(theme_i[j],"rgb(214, 167, 105)")});
+            theme_i[j].addEventListener("mouseout", function(){theme_btn_clicked_class(theme_i[j],color1)});
+        }
+    }
+}
+
+function theme_btn_clicked_id(id,colore1){
+    document.getElementById(id).style.backgroundColor=colore1;
+}
+function theme_btn_clicked_class(class1,colore1){
+    class1.style.background=colore1;
 }
