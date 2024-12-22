f4e5d5c360a505f0a054fc5f4fcfe6a709b615b1
LukaPrlenda <luka.prlenda@stu.ibu.edu.ba>
2024-12-22 16:14:34 +0100

    Added comments

--- YCwebsite_Services.js
+++ YCwebsite_Services.js
@@ -115,3 +115,105 @@ async function punat_t() {
         }
     }
 }
+
+
+
+async function comments(){
+    const comments=document.getElementById("comments");
+    let all_comments="";
+
+    try{
+        const response = await fetch("Ycwebsite_Your_Feedback.json");
+        const data = await response.json();
+
+        if(response.ok){
+            console.log("Comment data successfully fetched.");
+        }
+
+        for(let i in data){
+            all_comments+="<tr class='table3_comments'><td>"+data[i]+"</td></tr>";
+        }
+        comments.innerHTML=all_comments;
+    }
+    catch(error){
+        console.log("Error geting comment data:",error);
+    }
+}
+
+comments();
+
+
+let comment_edit=0;
+
+document.getElementById("feedback_btn").addEventListener("click", function(){edit_comments();});
+
+function edit_comments(){
+    const actions=document.getElementById("actions");
+    
+    if(comment_edit===0){
+        actions.innerHTML="<tr><td id='add_comment'>Add comment</td></tr><tr><td><form id='comment_input'><input class='comment_input' type='text' name='new_comment' placeholder='Enter your comment'></form></td></tr><tr><td id='delete_comment'>Delete comment</td></tr>";
+        const add=document.getElementById("add_comment");
+        const del=document.getElementById("delete_comment");
+
+        add.addEventListener("mouseover", function(){add.style.color="darkgreen"; add.style.backgroundColor="mediumseagreen";});
+        del.addEventListener("mouseover", function(){del.style.color="darkred"; del.style.backgroundColor="crimson";});
+        add.addEventListener("mouseout", function(){add.style.color="navy"; add.style.backgroundColor="whitesmoke";});
+        del.addEventListener("mouseout", function(){del.style.color="navy"; del.style.backgroundColor="whitesmoke";});
+
+        add.addEventListener("click", function(){adding();});
+        del.addEventListener("click", function(){dele();});
+
+        comment_edit=1;
+    }
+    else{
+        actions.innerHTML="";
+        
+        comment_edit=0;
+    }
+}
+
+async function adding(){
+    const adding_com=document.getElementById("adding_com");
+
+    try{
+        const comment_data=await new FormData(document.getElementById("comment_input"));
+        const new_comment=await comment_data.get("new_comment");
+        
+        if(new_comment===""){
+            console.log("Error: empty comment can not be added.");
+            document.getElementById("error1_red").style.display="block";
+            setTimeout( function(){document.getElementById("error1_red").style.display="none";}, 3000);
+        }
+        else{
+            adding_com.innerHTML="<tr id='can_del' class='table3_comments'><td>"+new_comment+"</td></tr>";
+            adding_com.style.color="navy"; adding_com.style.backgroundColor="whitesmoke";
+            document.getElementById("add_green").style.display="block";
+            setTimeout( function(){document.getElementById("add_green").style.display="none";}, 2500);
+        }
+    }
+    catch(error){
+        console.log("Error geting comment input:",error);
+    }
+    
+}
+
+
+function dele(){
+    const del=document.getElementById("adding_com");
+    const to_del=document.getElementById('can_del');
+
+    to_del.addEventListener("mouseover", function(){to_del.style.color="darkred"; to_del.style.backgroundColor="crimson";});
+    to_del.addEventListener("mouseout", function(){to_del.style.color="darkred"; to_del.style.backgroundColor="rgb(237, 112, 112)";});
+
+    to_del.addEventListener("click", function(){deling();});
+
+    del.style.color="darkred"; del.style.backgroundColor="rgb(237, 112, 112)";
+}
+
+function deling(){
+    const adding_com=document.getElementById("adding_com");
+
+    adding_com.innerHTML="";
+    document.getElementById("del_green").style.display="block";
+    setTimeout( function(){document.getElementById("del_green").style.display="none";}, 2500);
+}
